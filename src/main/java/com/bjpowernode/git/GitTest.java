2030caec6aa163a22506305039e55f434a6c9e29
tangzhen <tangzhen020515@163.com>
2023-12-24 16:53:20 +0800

    first test

---
+++ src/main/java/com/bjpowernode/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.bjpowernode.git;
+
+/**
+ * @author tangzhen
+ * @version 1.0
+ * @date 2023/12/24 16:52
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hhhh");
+    }
+}
