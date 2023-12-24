30b0a028efd3a9c1b95cab5320cf388b549ce207
tangzhen <tangzhen020515@163.com>
2023-12-24 17:04:23 +0800

    hot-fix FIRST commit

--- src/main/java/com/bjpowernode/git/GitTest.java
+++ src/main/java/com/bjpowernode/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hhhh");
         System.out.println("Fuck U!");
+        System.out.println("hot-fix1111");
     }
 }
